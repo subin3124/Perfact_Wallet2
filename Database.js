4c6f8eea8a5c81633559b4af311f636a93c1e1b8
kimsubin <subibi21@inu.ac.kr>
2023-11-02 01:21:21 +0900

    add logging

--- Database.js
+++ Database.js
@@ -7,10 +7,10 @@ class Database{
     Insert(data) {
         // data = [{item : '라면', qu : 1, cost : 3500}]
         for(let i in data) {
-            this.db.run(`INSERT INTO perfect_wallet_DB (item, qu, cost) VALUES ("${data[i].item}", ${data[i].qu}, ${data[i].cost})`, (err) => {
+            this.db.run(`INSERT INTO perfect_wallet_DB (item, qu, cost) VALUES ("${data[i].objectName}", ${data[i].qu}, ${data[i].cost})`, (err) => {
                 if(err)
                     console.error(err.message);
-                console.log(`inserted data ${data[i].item}`);
+                console.log(`inserted data ${data[i].objectName}`);
             });
         }
     }
