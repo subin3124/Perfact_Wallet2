a30c05684214148b1bd149bf4916e91882fb04fd
kimsubin <subibi21@inu.ac.kr>
2023-11-06 02:38:01 +0900

    backend fixed

--- ReceiptItemRepository.js
+++ ReceiptItemRepository.js
@@ -11,7 +11,7 @@ class ReceiptItemRepository {
     Insert(data) {
         // data = [{item : '라면', qu : 1, cost : 3500}]
         for(let i in data) {
-            this.db.run(`INSERT INTO Item (item, qu, cost, ReceiptID) VALUES ('${data[i].objectName}', ${data[i].qu}, ${data[i].price}, '${data[i].ReceiptID}'`, (err) => {
+            this.db.run(`INSERT INTO Item (item, qu, cost, ReceiptID) VALUES ('${data[i].objectName}', ${data[i].qu}, ${data[i].price}, '${data[i].ReceiptID}')`, (err) => {
                 if(err)
                     console.error(err.message);
                 console.log(`inserted data ${data[i]}`);
